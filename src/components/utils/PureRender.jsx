ef050ec2c1976ac0d7b51094c02dbb34b202879f
Leo Bonnargent <leo.bonnargent@fr.ey.com>
2019-12-13 08:38:18 +0100

    remove debug console.log

--- src/components/utils/PureRender.jsx
+++ src/components/utils/PureRender.jsx
@@ -4,11 +4,6 @@ import { isEqual } from 'lodash';
 export default class PureRenderComponent extends Component {
   shouldComponentUpdate(nextProps) {
     const shouldRender = !isEqual(this.props, nextProps);
-
-    if (!shouldRender) {
-      console.log('prevent rendering', this.displayName);
-    }
-
     return shouldRender;
   }
 }
